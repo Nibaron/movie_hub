0a9e6a519a45e6534b686e8e457eadbc6e0297f1
Nibaron Kumar Das <36547410+Nibaron@users.noreply.github.com>
2023-09-04 22:22:32 +0600

    footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,39 +1,57 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const Footer = () => {
     return (
-        <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
-            <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-                    © 2023{" "}
-                    <Link to="/" className="hover:underline">
-                        MovieHUB™
-                    </Link>
-                    . All Rights Reserved.
-                </span>
-                <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-                    <li>
-                        <NavLink to="#" className="mr-4 hover:underline md:mr-6 ">
-                            About
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="#" className="mr-4 hover:underline md:mr-6">
-                            Privacy Policy
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="#" className="mr-4 hover:underline md:mr-6">
-                            Licensing
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="#" className="hover:underline">
-                            Contact
-                        </NavLink>
-                    </li>
-                </ul>
-            </div>
+        <footer className="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+                © 2023{" "}
+                <Link to="/" className="hover:underline">
+                    Movie HUB
+                </Link>
+                . All Rights Reserved.
+            </span>
+            <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+                <li>
+                    <a
+                        href="https://www.facebook.com/nibaron.kumar2"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="mr-4 hover:underline md:mr-6 "
+                    >
+                        Facebook
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="https://www.linkedin.com/in/nibaron-ruet/"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="mr-4 hover:underline md:mr-6"
+                    >
+                        LinkedIn
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="https://github.com/Nibaron"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="mr-4 hover:underline md:mr-6"
+                    >
+                        Github
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="https://leetcode.com/Nibaron_RUET/"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="hover:underline"
+                    >
+                        LeetCode
+                    </a>
+                </li>
+            </ul>
         </footer>
     );
 };
