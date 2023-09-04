fab8fe5746ba62bcc7bb65196c678161ac20f248
Nibaron Kumar Das <36547410+Nibaron@users.noreply.github.com>
2023-09-04 19:10:04 +0600

    mobile search and sidebar functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 import { Link, NavLink } from "react-router-dom";
 import logo from "../assets/video-camera_9776807.png";
+import { useState } from "react";
 
 export const Header = () => {
+    const [hidden, setHidden] = useState(true);
     const activeHeader =
         "text-base block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500";
     const inactiveHeader =
@@ -18,6 +20,7 @@ export const Header = () => {
                     </Link>
                     <div className="flex md:order-2">
                         <button
+                            onClick={()=> setHidden(!hidden)}
                             type="button"
                             data-collapse-toggle="navbar-search"
                             aria-controls="navbar-search"
@@ -41,6 +44,7 @@ export const Header = () => {
                             </svg>
                             <span className="sr-only">Search</span>
                         </button>
+
                         <div className="relative hidden md:block">
                             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                                 <svg
@@ -65,14 +69,17 @@ export const Header = () => {
                                 id="search-navbar"
                                 className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="Search..."
+                                autoComplete="off"
                             />
                         </div>
                         <button
+                        onClick={()=> setHidden(!hidden)}
                             data-collapse-toggle="navbar-search"
                             type="button"
                             className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                             aria-controls="navbar-search"
                             aria-expanded="false"
+                            
                         >
                             <span className="sr-only">Open main menu</span>
                             <svg
@@ -93,7 +100,7 @@ export const Header = () => {
                         </button>
                     </div>
                     <div
-                        className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+                        className={` ${ hidden ? "hidden" : " " } items-center justify-between w-full md:flex md:w-auto md:order-1`}
                         id="navbar-search"
                     >
                         <div className="relative mt-3 md:hidden">
@@ -119,6 +126,7 @@ export const Header = () => {
                                 id="search-navbar"
                                 className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="Search..."
+                                autoComplete="off"
                             />
                         </div>
                         <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
@@ -171,3 +179,4 @@ export const Header = () => {
         </header>
     );
 };
+
