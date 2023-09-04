3ebd14eb4e3afbf461b3f5658d8231f890723608
Nibaron Kumar Das <36547410+Nibaron@users.noreply.github.com>
2023-09-04 17:53:56 +0600

    active Header added

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,16 @@ import { Link, NavLink } from "react-router-dom";
 import logo from "../assets/video-camera_9776807.png";
 
 export const Header = () => {
+    const activeHeader =
+        "text-base block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500";
+    const inactiveHeader =
+        "text-base block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700";
     return (
         <header>
             <nav className="bg-white border-gray-200 dark:bg-gray-900">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                    <Link
-                        to="/"
-                        className="flex items-center"
-                    >
-                        <img src={logo} alt='logo' />
+                    <Link to="/" className="flex items-center">
+                        <img src={logo} alt="logo" />
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
                             Movie HUB
                         </span>
@@ -123,25 +124,42 @@ export const Header = () => {
                         <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                             <li>
                                 <NavLink
-                                    to="#"
-                                    className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                                    to="/"
+                                    className={({ isActive }) =>
+                                        isActive ? activeHeader : inactiveHeader
+                                    }
                                     aria-current="page"
                                 >
                                     Home
                                 </NavLink>
                             </li>
+
                             <li>
                                 <NavLink
-                                    to="#"
-                                    className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                                    to="/popular"
+                                    className={({ isActive }) =>
+                                        isActive ? activeHeader : inactiveHeader
+                                    }
+                                >
+                                    popular
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to="/top"
+                                    className={({ isActive }) =>
+                                        isActive ? activeHeader : inactiveHeader
+                                    }
                                 >
                                     Top
                                 </NavLink>
                             </li>
                             <li>
                                 <NavLink
-                                    to="#"
-                                    className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                                    to="/upcoming"
+                                    className={({ isActive }) =>
+                                        isActive ? activeHeader : inactiveHeader
+                                    }
                                 >
                                     Upcoming
                                 </NavLink>
